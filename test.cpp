6ade10d6af824e2e4b340ce62c13dbdf022d2e8f
Zarman-oss <zarmanimran@gmail.com>
2023-11-19 09:51:34 +0500

    first commit

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *p;
+    int x[6];
+    x[0] = 1;
+    x[1] = 2;
+    x[2] = 3;
+    x[3] = 4;
+    x[4] = 4;
+    x[5] = 5;
+
+    cout
+        << "x";
+    return 0;
+}
