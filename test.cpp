a3ad879250e61bf658a260fbacd23457aa129334
Zarman-oss <zarmanimran@gmail.com>
2023-11-20 19:01:09 +0500

    ,

--- test.cpp
+++ test.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 using namespace std;
 
-class Node
-{
-public:
-    int data;   // to save the value
-    Node *next; // to point the address in index
-
-}
-
 main()
 {
+    int x[5] = {1, 2, 3, 4, 5};
+    int found = 5, search_value;
+    cout << "Enter the value";
+    cin >> search_value;
+    for (int i = 0; int <= 4; i++)
+    {
+        if (search_value == x[i])
+        {
+            cout << "Value is found...";
+            found++;
+            break;
+        }
+        if (found == 0)
+        {
+            cout << "n\n Value can't be found";
+        }
+        return x[current]
+    }
 
-    cout
-        << "x";
     return 0;
 }
