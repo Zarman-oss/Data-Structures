42b9c99c99b34e4b8e7f176396a0056de695527d
Zarman-oss <zarmanimran@gmail.com>
2023-11-27 21:51:06 +0500

    .

--- test.cpp
+++ test.cpp
@@ -3,24 +3,9 @@ using namespace std;
 
 main()
 {
-    int x[5] = {1, 2, 3, 4, 5};
-    int found = 5, search_value;
-    cout << "Enter the value";
-    cin >> search_value;
-    for (int i = 0; int 4; i++)
-    {
-        if (search_value == x[i])
-        {
-            cout << "Value is found...";
-            found++;
-            break;
-        }
-        if (found == 0)
-        {
-            cout << "n\n Value can't be found";
-        }
-        return 0;
-    }
+    int x;
+
+    cout << "x";
 
     return 0;
 }
