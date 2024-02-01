36fae29c9ba06ba63f47ba7b56f5dc589ca6f67a
Zarman-oss <zarmanimran@gmail.com>
2024-02-01 05:38:04 +0500

    .

--- test.cpp
+++ test.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 
-using namespace std;
-//
-
-int isFull()
-{
-    return noElements == size;
-}
-
 int main()
-
 {
-    int n = 100;
-    displayNumbers(n);
-
-    return 0;
+    std::cout << "Hello World" << std::endl;
 }
