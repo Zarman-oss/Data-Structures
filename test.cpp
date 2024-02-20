50cf5a1c9346706c1e67b1d78bbc270c62d7a8e6
Zarman-oss <zarmanimran@gmail.com>
2024-02-20 20:59:03 +0500

    .

--- test.cpp
+++ test.cpp
@@ -2,22 +2,21 @@
 #include <fstream>
 using namespace std;
 
-class Faculty
-{
-private:
-  string name;
-  int age;
-  string post;
-
-public:
-  void setName(int n)
-  {
-    name = n;
-  }
-};
-
 int main()
 {
+  int x[] = {2, 4, 5, 6, 7, 8, 9, 10};
+  int size = sizeof(x);
+  for (int i = 0; i < size++ i)
+  {
+    cout << x[i];
+  }
 
+  for (int i = 0; i < size; ++i)
+  {
+    if (x[i] % 2 == 0)
+    {
+      x[i] = x[i] * x[i];
+    }
+  }
   return 0;
 }
